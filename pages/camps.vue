04617b0b76fcd71942f8eaf740b11d4a077e2727
Jasen Peterson <misc@jasenpeterson.com>
2017-12-11 08:16:20 -0600

    Camps Page

--- pages/camps.vue
+++ pages/camps.vue
@@ -7,12 +7,27 @@
           <div class="cell" v-html="data.acf.row_one.column_two"></div>
         </div>
       </div>
+      <div class="row row__two" v-for="(data, index) in pageData" v-bind:key="index">
+        <div class="cell camp__description__wrap" v-for="(data, index) in data.acf.row_two.camps" v-bind:key="index">
+          <article>
+            <h1>{{data.content.post_title}}</h1>
+            <p>{{$options.filters.truncate(data.content.post_content, '200')}}</p>
+            <nuxt-link class="btn" :to="data.content.post_name">Learn More</nuxt-link>
+          </article>
+        </div>
+      </div>
+      <div class="row row__three">
+        <div class="content__wrap" v-for="(data, index) in pageData" v-bind:key="index">
+          <div class="cell" v-html="data.acf.row_three.content"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import NuxtLink from '../.nuxt/components/nuxt-link'
 import { mapGetters } from 'vuex'
 export default {
   async asyncData ({ store, params }) {
@@ -37,6 +52,11 @@ export default {
   data () {
     return {
       title: 'default'
+    }
+  },
+  filters: {
+    truncate: function (string, value) {
+      return string.substring(0, value) + '...'
     }
   },
   computed: {
@@ -98,6 +118,59 @@ export default {
 
 .row {
   padding: 0 2rem;
+}
+
+.row__two {
+  display: flex;
+  justify-content: center;
+  margin: 12rem 0;
+}
+
+.row__two,
+.row__three  {
+  padding: 0 4rem;
+}
+
+.row__two .cell {
+  width: 33.3%;
+}
+
+.row__three .cell {
+  width: 73.75rem;
+  color: #F05449;
+  font-size: 2rem;
+}
+
+.camp__description__wrap {
+  color: #6C6D71;
+}
+
+.camp__description__wrap h1 {
+  color: #FCCD00;
+  text-transform: uppercase;
+  font-size: 2.5rem;
+  font-weight: 900;
+  line-height: 2.8rem;
+}
+
+.camp__description__wrap article {
+  position: relative;
+  padding-left: 3.5rem;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.camp__description__wrap article:before {
+  content: url(/static/icons/balloon__man__circle.svg);
+  display: block;
+  width: 4.5rem;
+  height: auto;
+  max-width: 100%;
+  position: absolute;
+  top: -2.5rem;
+  left: -1.5rem;
 }
 
 </style>
